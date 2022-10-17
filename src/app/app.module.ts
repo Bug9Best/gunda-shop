6cab32aa6054507bcd32e1ce9a65d6fb3923e993
Bug9Best <Bug9Best@gmail.com>
2022-10-17 20:26:31 +0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
-import { CartComponent } from './cart/cart.component';
-import { AccountComponent } from './account/account.component';
-import { SearchComponent } from './search/search.component';
-import { IntroComponent } from './intro/intro.component';
-import { HighGradeComponent } from './category/high-grade/high-grade.component';
-import { RealGradeComponent } from './category/real-grade/real-grade.component';
-import { MasterGradeComponent } from './category/master-grade/master-grade.component';
-import { PerfectGradeComponent } from './category/perfect-grade/perfect-grade.component';
-import { ToolComponent } from './category/tool/tool.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SigninComponent,
-    SignupComponent,
-    CartComponent,
-    AccountComponent,
-    SearchComponent,
-    IntroComponent,
-    HighGradeComponent,
-    RealGradeComponent,
-    MasterGradeComponent,
-    PerfectGradeComponent,
-    ToolComponent
   ],
   imports: [
     BrowserModule,
