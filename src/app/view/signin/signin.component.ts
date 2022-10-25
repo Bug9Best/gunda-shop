cd761ba9d468da68f501bf6bd317e83ab9b32e66
Bug9Best <Bug9Best@gmail.com>
2022-10-25 22:02:18 +0700

    Add Validate

--- src/app/view/signin/signin.component.ts
+++ src/app/view/signin/signin.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenService } from 'src/app/service/authen/authen.service';
+import { Router } from '@angular/router';
 import {
   Auth,
   signInWithEmailAndPassword,
   isSignInWithEmailLink,
   createUserWithEmailAndPassword
 } from '@angular/fire/auth';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  NonNullableFormBuilder
+} from '@angular/forms';
+import { getDocs, Firestore, collection } from '@angular/fire/firestore';
+import { MessageService } from 'primeng/api';
+
+
 
 @Component({
   selector: 'app-signin',
@@ -12,19 +24,50 @@ import {
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  showValid = true;
 
-  constructor(public auth: Auth) { }
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required,Validators.minLength(8)]),
+  });
+
+
+  constructor(
+    public auth: Auth,
+    private authenService: AuthenService,
+    private router: Router,
+    private messageService: MessageService,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  checkUser(value: any) {
-    createUserWithEmailAndPassword(this.auth, value.email, value.password)
-      .then((response: any) => {
-        console.log(response.user);
-      })
-      .catch((err) => {
-        alert(err.message);
-      })
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  login() {
+    const { email, password } = this.loginForm.value;
+
+    if (!this.loginForm.valid || !email || !password) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'อีเมลหรือรหัสผ่านไม่ถูกต้อง',
+        detail: 'ตรวจสอบอีเมลและรหัสผ่านอีกครั้ง',
+        life: 3000,
+      });
+      this.showValid = false;
+      return;
+    }
+
+    this.authenService
+      .signUp(email, password)
+      .subscribe(() => {
+        this.router.navigate(['/home']);
+      });
   }
 }
