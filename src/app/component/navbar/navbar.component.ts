0a6617d4b1accae9fcf417df2c3536a54450686e
Bug9Best <Bug9Best@gmail.com>
2022-10-26 07:13:00 +0700

    Add state user

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user/user.service';
+import { AuthenService } from 'src/app/service/authen/authen.service';
+import { getDocs, Firestore, collection } from '@angular/fire/firestore';
+import { User } from 'src/app/model/user';
+import { first } from 'rxjs';
+
+
 
 @Component({
   selector: 'navbar',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  user$ = this.usersService.getCurrentUser();
 
-  constructor() { }
+  userData: User = {
+    uid: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    photoURL: '',
+  };
+
+  constructor(
+    public usersService: UserService,
+    public authenService: AuthenService,
+    public firestore: Firestore
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.user$.subscribe((user) => {
+      this.userData.uid = user?.uid || '';
+      this.userData.firstname = user?.firstname;
+      this.userData.lastname = user?.lastname;
+      this.userData.email = user?.email;
+      this.userData.photoURL = user?.photoURL;
+    });
 
+    console.log(this.userData);
+  }
 }
