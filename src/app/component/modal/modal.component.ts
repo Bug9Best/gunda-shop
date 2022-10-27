2ba6a8764932357eed0875fa2f7a9560add5918b
Bug9Best <Bug9Best@gmail.com>
2022-10-27 07:05:37 +0700

    Not Done

--- src/app/component/modal/modal.component.ts
+++ src/app/component/modal/modal.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostListener, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
-  selector: 'app-modal',
+  selector: 'modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
 
+  @Input() display?: boolean;
+  @Output() closeDialog = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  close() {
+    return this.closeDialog.emit();
+  }
 }
