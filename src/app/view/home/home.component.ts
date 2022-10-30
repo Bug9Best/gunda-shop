6e1563a6c1073f010ba101637acd8eaecde371b7
Bug9Best <Bug9Best@gmail.com>
2022-10-30 14:10:19 +0700

    Update

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -11,22 +11,75 @@ import { query, orderBy, limit } from "firebase/firestore";
 export class HomeComponent implements OnInit {
   imgList: any = [];
 
+  highGradeList: any = [];
+  realGradeList: any = [];
+  masterGradeList: any = [];
+  perfectGradeList: any = [];
+
+  randomNum: number = 0;
+
   constructor(
     private firestore: Firestore,
   ) {
-    this.getImage();
+    // this.getImage();
+    this.getHighGrade();
+    this.getRealGrade();
+    this.getMasterGrade();
+    this.getPerfectGrade();
+    this.random();
   }
 
   ngOnInit(): void {
   }
 
-  getImage() {
-    const firebase = collection(this.firestore, 'assets');
+  // getImage() {
+  //   const firebase = collection(this.firestore, 'assets');
+  //   getDocs(firebase).then((response) => {
+  //     this.imgList = [...response.docs.map((item) => {
+  //       return { ...item.data(), id: item.id }
+  //     })]
+  //     console.log(this.imgList[0].src);
+  //   })
+  // }
+
+  getHighGrade() {
+    const firebase = collection(this.firestore, 'high-grade');
     getDocs(firebase).then((response) => {
-      this.imgList = [...response.docs.map((item) => {
-        return { ...item.data(), id: item.id }
-      })]
-      console.log(this.imgList[0].src);
+      response.docs.map((item) => {
+        this.highGradeList.push(item.data()['productURL']);
+      })
     })
+  }
+
+  getRealGrade() {
+    const firebase = collection(this.firestore, 'real-grade');
+    getDocs(firebase).then((response) => {
+      response.docs.map((item) => {
+        this.realGradeList.push(item.data()['productURL']);
+      })
+    })
+  }
+
+  getMasterGrade() {
+    const firebase = collection(this.firestore, 'master-grade');
+    getDocs(firebase).then((response) => {
+      response.docs.map((item) => {
+        this.masterGradeList.push(item.data()['productURL']);
+      })
+    })
+  }
+
+  getPerfectGrade() {
+    const firebase = collection(this.firestore, 'perfect-grade');
+    getDocs(firebase).then((response) => {
+      response.docs.map((item) => {
+        this.perfectGradeList.push(item.data()['productURL']);
+      })
+    })
+  }
+
+  random() {
+    this.randomNum = Math.floor(Math.random() * 16);
+    console.log(this.randomNum);
   }
 }
