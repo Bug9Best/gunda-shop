42f6e288fbd372a6740c84b1eb3a7140669f4456
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2022-10-30 11:48:48 +0700

    Cart have item
    Just back end
    font is done.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
+    }
+    
   ngOnInit(): void {
   }
 
 }
+
